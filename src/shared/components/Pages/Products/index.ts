8d40570b661fe84f7e2432a98dda479e9763d7c1
console1928 <riverflow1928@yandex.ru>
2025-10-01 01:52:17 +0300

    fix build errors

--- src/shared/components/Pages/Products/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Products';
-
-export { default } from './Products';
-export { default as ProductsHeader } from './ProductsHeader';
-export { default as ProductsSearch } from './ProductsSearch';
-export { default as ProductsContent } from './ProductsContent';
